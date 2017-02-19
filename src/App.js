5047c9c0c4c6a8a6df25bf5ed2cae1357d0bb60f
Chris Mielke <chris@chrismielke.com>
2017-02-19 11:14:12 -0800

    Convert auth to use redirection instead of popup for better mobile compatibility

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
   promptLogin() {
     const provider = new firebase.auth.FacebookAuthProvider()
 
-    firebase.auth().signInWithPopup(provider).then(function(result) {
+    firebase.auth().signInWithRedirect(provider).then(function(result) {
       // // This gives you a Facebook Access Token. You can use it to access the Facebook API.
       // var token = result.credential.accessToken;
       // // The signed-in user info.
